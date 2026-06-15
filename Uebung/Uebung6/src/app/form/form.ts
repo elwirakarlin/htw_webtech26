1958807dc24bcc7c82ec72a4895d59c68e8c3b93
Elwira <elwira.karlin@gmail.com>
2026-06-15 13:47:54 +0200

    15 Juni, die Uebung 6 zu Ende gemacht, Attritbut und Event Binding"

--- Uebung/Uebung6/src/app/form/form.ts
+++ Uebung/Uebung6/src/app/form/form.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
-export class Form {}
+// 4 Variblen in TS Klasse, die werden in html eingebunden
+
+export class Form {
+
+  vornameControl = new FormControl(" ", Validators.required);
+  nachnameControl = new FormControl(" ");
+  emailControl = new FormControl(" ");
+  ipadresseControl = new FormControl (" ");
+
+  create(): void{
+
+const vorname = this.vornameControl.value || ''; // mit || stellen wir klar, dass es ein Wert hat ODER aber einen leeeren String 
+const nachname = this.nachnameControl.value;
+const email = this.emailControl.value;
+const ipAdress = this.ipadresseControl.value; //lokale Variablen
+
+
+const newUser = {vorname,nachname,email,ipAdress,name: () => { vorname + nachname}};
+
+// vorname ! - ich übernehmen die verantwortung, dass es nicht null ist
+
+console.log('Creating entry:', newUser); //Objektvariable = newUser mit Eigenschaften die aufgezählt werden
+
+  }
+
+inValid (): boolean {
+    
+  return this.vornameControl.value != '' &&
+  this.nachnameControl.value != '' &&
+  this.emailControl.value != '' &&
+  this.ipadresseControl.value != ''
+
+  }
+
+}
