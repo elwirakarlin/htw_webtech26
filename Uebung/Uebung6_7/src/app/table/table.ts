060758e57634b3c5e7ac6960a054c4f5f7469d3c
Elwira <elwira.karlin@gmail.com>
2026-07-07 21:47:18 +0200

    Uebung 6, Uebung 7 als Ergänzung von 6 und Uebung 8 = anbinden von DB, (MongoDb, Atlas, Compass,)

---
+++ Uebung/Uebung6_7/src/app/table/table.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Dataservice } from '../shared/dataservice';
+import { Member } from '../shared/member';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-table',
+  imports: [ReactiveFormsModule],
+  templateUrl: './table.html',
+  styleUrl: './table.css',
+})
+export class Table implements OnInit{
+  
+  memberservice = inject(Dataservice)
+  members = signal<Member[]>([]);
+  filterMembers = signal<Member[]>([]);
+
+  search = new FormControl('')
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.memberservice.getAllMembers() //einmal service aufrufen, um alle Members zu holen -> da teuer!
+    this.members.set(data)
+    this.filterMembers.set(this.members())
+    console.log('members in table : ', this.members())
+  }
+
+  filter() {
+    console.log('in filter', this.search.value)
+    const value = this.search.value?.toLowerCase() || ''; //solle value null sein, dann nehmen wir den leeren string
+    this.filterMembers.set(this.members().filter( m=> 
+      m.forename.toLowerCase().includes(value) || 
+      m.surname.toLowerCase().includes(value) || 
+      m.email.toLowerCase().includes(value)) )
+  }
+
+}
